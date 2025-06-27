c9abfce6beec3deef155ee060e3159ea4e355401
Maruf <marufsherikar32@gmail.com>
2025-06-27 15:09:42 +0530

    do while loop

---
+++ src/basics_05/do_while.java
@@ -0,0 +1,57 @@
+package src.basics_05;
+
+public class do_while {
+    public static void main(String[] args) {
+
+        int a = 10;  // -> Initialization
+
+        do {
+            System.out.println(a);    // Update or body or SOP Statement
+            a++;   //   -> Increment or Decrement
+        }while (a<10);     // -> Condition
+
+
+
+
+
+        // Difference between while and do while is shown in both example
+        // for while loop no value is print if the condition is false
+        // for do while loop one time value will be print
+
+
+
+
+
+        // for loop
+
+        int i = 10;
+
+        while (i>10){
+            System.out.println(i);
+            i++;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
