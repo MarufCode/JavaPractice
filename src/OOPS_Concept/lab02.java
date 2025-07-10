ca4e919fa7b998678720e8d986d5c7fff6d57c80
Maruf <marufsherikar32@gmail.com>
2025-07-10 17:16:54 +0530

    OOPS Concept

---
+++ src/OOPS_Concept/lab02.java
@@ -0,0 +1,31 @@
+package src.OOPS_Concept;
+
+public class lab02 {
+    public static void main(String[] args) {
+
+        person p = new person();
+        p.name = "Amit";
+        System.out.println(p.name);
+
+        p.email = "AdsJbfhjoksf@.com";
+        System.out.println(p.email);
+
+        // person is -> class blueprint
+        // p  is -> reference
+        // new person is -> Object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/OOPS_Concept/person.java
@@ -0,0 +1,57 @@
+package src.OOPS_Concept;
+
+public class person {
+
+
+    // Attribute
+
+    String name;
+    int age;
+
+    String email;
+
+    long number;
+
+    double height;
+
+    String address;
+
+    String gender;
+
+
+
+
+    // Behaviour
+
+    void isTalk(){
+        System.out.println("Can talk");
+    }
+
+    void sleep(){
+        System.out.println("Wants to sleep");
+    }
+
+    Boolean isMarried(){
+       return true;
+    }
+
+
+    // OOPS Concept
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
