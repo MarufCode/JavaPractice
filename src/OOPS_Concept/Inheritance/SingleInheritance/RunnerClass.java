f06a54513bbe2bc574dcb9e300940cb77d4829b9
Maruf <marufsherikar32@gmail.com>
2025-07-16 16:36:25 +0530

    Changes Pushed

--- src/OOPS_Concept/Inheritance/SingleInheritance/RunnerClass.java
+++ src/OOPS_Concept/Inheritance/SingleInheritance/RunnerClass.java
@@ -1,5 +1,7 @@
 package src.OOPS_Concept.Inheritance.SingleInheritance;
 
+import java.util.Scanner;
+
 public class RunnerClass {
     public static void main(String[] args) {
 
@@ -7,6 +9,7 @@ public class RunnerClass {
        Son s = new Son();
         s.money();
         s.property();
+
 
 
     }
