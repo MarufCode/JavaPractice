5371ed4f141d880b584915f23116e6b7490c6358
Maruf <marufsherikar32@gmail.com>
2025-08-01 16:13:42 +0530

    set, Hashset, Treeset practiced

--- src/CollectionFramework/Set/lab34.java
+++ src/CollectionFramework/Set/lab34.java
@@ -1,6 +1,67 @@
 package src.CollectionFramework.Set;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class lab34 {
 
+    public static void main(String[] args) {
+
+        // Set:
+        // It stored only unique value
+        // Set is a interface
+        // Hashset Does not maintain any order.
+
+
+        Set fruits = new HashSet();
+        fruits.add("Orange");
+        fruits.add("Mango");
+        fruits.add("Watermelon");
+        fruits.add("apple");
+        fruits.add("Watermelon");  // Duplicate value is not allowed in set or Hashset
+
+//        System.out.println(fruits);
+//        System.out.println(fruits.isEmpty());
+//        System.out.println(fruits.size());
+//        System.out.println(fruits.contains("apple"));
+
+
+       Iterator it = fruits.iterator();
+           while (it.hasNext()){
+               System.out.println(it.next());
+           }
+
+
+        System.out.println("--------------------");
+
+        for (Object Obj : fruits){
+            System.out.println(Obj);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 }
---
+++ src/CollectionFramework/Set/lab35.java
@@ -0,0 +1,38 @@
+package src.CollectionFramework.Set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class lab35 {
+    public static void main(String[] args) {
+
+
+
+        Set fruits = new TreeSet();
+        fruits.add("Orange");
+        fruits.add("Mango");
+        fruits.add("Watermelon");
+        fruits.add("Apple");
+
+        fruits.add("Watermelon");
+        System.out.println(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
