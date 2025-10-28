f753b0b3e6c1cff862d1bff9e7ec5aebb16418b9
Maruf <marufsherikar32@gmail.com>
2025-10-28 11:06:07 +0530

    Interview program chanllenges

---
+++ src/interview_programs/challenge_01.java
@@ -0,0 +1,14 @@
+package src.interview_programs;
+
+public class challenge_01 {
+
+    public static void main(String[] args) {
+
+        boolean b = false;
+        if (b = !b){
+            System.out.println("Matched");
+        }else {
+            System.out.println("not matched");
+        }
+    }
+}
